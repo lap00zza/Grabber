aa22fd736ad9a8e2da7e5b6b5aea2bdc1ac6b705
Jewel Mahanta <jewelmahanta@gmail.com>
2017-06-29 14:00:14 +0530

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Grabber
+[![JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
+
+Grab everything!
---
+++ grabber.user.js
@@ -0,0 +1,192 @@
+// ==UserScript==
+// @name        Grabber
+// @namespace   https://github.com/lap00zza/
+// @version     0.1.0
+// @description Grab links from 9anime!
+// @author      Jewel Mahanta
+// @icon        https://image.ibb.co/fnOY7k/icon48.png
+// @match       *://9anime.to/watch/*
+// @match       *://9anime.is/watch/*
+// @match       *://9anime.tv/watch/*
+// @grant       GM_addStyle
+// @grant       GM_xmlhttpRequest
+// @license     MIT License
+// ==/UserScript==
+(function () {
+  'use strict'
+  console.log('Grabber ' + window.GM_info.script.version + ' is now running!')
+  var dlInProgress = false // global switch to indicate dl status
+  var dlEpisodeIds = [] // list of id's currently being downloaded
+  var dlServerType = '' // FIXME: cant queue different server types together
+  var dlAggregateLinks = '' // stores all the download links as a single string
+  var ts = document.getElementsByTagName('body')[0].dataset['ts'] // ts is needed to send API requests
+
+  // Apply styles
+  var styles = [
+    '.grabber__btn {',
+    '    border: 1px solid #555;',
+    '    border-radius: 2px;',
+    '    background-color: #16151c;',
+    '    margin-top: 5px;}',
+    '.grabber__btn:hover {',
+    '    background-color: #111111;}',
+    '.grabber__btn:active {',
+    '    background-color: #151515;}'
+  ]
+  window.GM_addStyle(styles.join(''))
+
+  /********************************************************************************************************************/
+  // Token generation scheme for 9anime. The token
+  // is used for _ value when sending any API requests.
+  const DD = 'gIXCaNh' // This might change in the future
+
+  function s (t) {
+    var e
+    var i = 0
+    for (e = 0; e < t.length; e++) {
+      i += t.charCodeAt(e) * e + e
+    }
+    return i
+  }
+
+  function a (t, e) {
+    var i
+    var n = 0
+    for (i = 0; i < Math.max(t.length, e.length); i++) {
+      n += i < e.length ? e.charCodeAt(i) : 0
+      n += i < t.length ? t.charCodeAt(i) : 0
+    }
+    return Number(n).toString(16)
+  }
+
+  function generateToken (data, initialState) {
+    var keys = Object.keys(data)
+    var _ = s(DD) + (initialState || 0)
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i]
+      var trans = a(DD + key, data[key].toString())
+      _ += s(trans)
+    }
+    return _
+  }
+
+  /********************************************************************************************************************/
+  /**
+   * This function fetches the RapidVideo page, does regex matching
+   * and gets the video links.
+   * @param {string} url - The RapidVideo url to download videos
+   */
+  function getVideoLinksRV (url) {
+    var re = /("sources": \[)(.*)(}])/g
+
+    // We are using GM_xmlhttpRequest since we need to make
+    // cross origin requests.
+    window.GM_xmlhttpRequest({
+      method: 'GET',
+      url: url,
+      onload: function (response) {
+        var blob = response.responseText.match(re)[0]
+
+        var parsed = JSON.parse('{' + blob + '}')
+        dlAggregateLinks += parsed['sources'][0]['file'] + '\n'
+      }
+    })
+  }
+
+  /**
+   * Get the grabber info from the 9anime API.
+   * @param {string} qParams
+   *    A list of query parameters to send to the API.
+   */
+  function getGrabber (qParams) {
+    console.log(qParams)
+    var xhr = new window.XMLHttpRequest()
+    xhr.open('GET', '/ajax/episode/info?' + qParams, true)
+    xhr.onload = function () {
+      if (this.status === 200) {
+        if (dlServerType === 'RapidVideo') {
+          getVideoLinksRV(JSON.parse(this.responseText)['target'])
+        }
+      }
+    }
+    xhr.send()
+  }
+
+  /***
+   * Handles the grabbing process. Runs every 2 seconds to avoid
+   * overloading the 9anime API and/or getting our IP flagged as
+   * bot.
+   */
+  function processGrabber () {
+    var epId = dlEpisodeIds.shift()
+    console.log('Downloading: ', epId)
+    var data = {
+      ts: ts,
+      id: epId,
+      update: 0
+    }
+    data['_'] = generateToken(data)
+
+    // Generate the query parameters
+    var qParams = ''
+    var dKeys = Object.keys(data)
+    for (var i = 0; i < dKeys.length; i++) {
+      if (i === 0) {
+        qParams += dKeys[i] + '=' + data[dKeys[i]]
+      } else {
+        qParams += '&' + dKeys[i] + '=' + data[dKeys[i]]
+      }
+    }
+    getGrabber(qParams)
+    if (dlEpisodeIds.length !== 0) {
+      window.dlTimeout = setTimeout(processGrabber, 2000)
+    } else {
+      clearTimeout(window.dlTimeout)
+      dlInProgress = false
+      console.log('Grabber: completed')
+      console.log(dlAggregateLinks)
+    }
+  }
+
+  /***
+   * Generates a nice looking 'Download All' button that are
+   * added below the server labels.
+   * @param {string} type
+   *    The server type to generate this button for. Example:
+   *    RapidVideo, Openload etc. Currently only support
+   *    grabbing RapidVideo links.
+   * @returns {Element}
+   *    Download All button for specified server
+   */
+  function generateDlBtn (type) {
+    var dlBtn = document.createElement('button')
+    dlBtn.dataset['type'] = type
+    dlBtn.classList.add('grabber__btn')
+    dlBtn.appendChild(document.createTextNode('Grab All'))
+    dlBtn.addEventListener('click', function () {
+      var serverDiv = this.parentNode.parentNode
+      var epLinks = serverDiv.getElementsByTagName('a')
+      for (var i = 0; i < epLinks.length; i++) {
+        dlEpisodeIds.push(epLinks[i].dataset['id'])
+      }
+      if (!dlInProgress) {
+        console.log('Grabber: starting grabber...')
+        dlServerType = this.dataset['type']
+        dlInProgress = true
+        dlAggregateLinks = ''
+        processGrabber()
+      }
+    })
+    return dlBtn
+  }
+
+  // Attach the download button to RapidVideo for now.
+  var serverLabels = document.querySelectorAll('.server.row > label')
+  for (var i = 0; i < serverLabels.length; i++) {
+    var serverLabel = serverLabels[i].innerText.trim()
+    if (/^RapidVideo$/i.test(serverLabel)) {
+      console.log(serverLabels[i])
+      serverLabels[i].appendChild(generateDlBtn('RapidVideo'))
+    }
+  }
+})()
